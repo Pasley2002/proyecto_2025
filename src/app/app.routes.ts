1e40792f632261ea7de8cab50370678bd0a59ae2
Pasley2002 <valdes.matias.2020@gmail.com>
2025-05-05 21:32:39 -0300

    Actualización

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductoComponent } from './producto/producto.component';
+import { ListaProductosComponent } from './Paginas/lista-productos/lista-productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ProductoComponent},
+    {path: 'producto', component: ProductoComponent},
+    {path: 'producto/:idProducto', component: ProductoComponent},
+    {path: 'listaProducto', component: ListaProductosComponent}
+];
---
+++ src/app/producto/producto.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ActivatedRoute } from '@angular/router';
+import { ProductoService } from '../servicios/producto.service';
+
+@Component({
+  selector: 'app-producto',
+  imports: [FormsModule, CommonModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './producto.component.html',
+  styleUrl: './producto.component.css'
+})
+export class ProductoComponent {
+  public miProducto!: Producto;
+
+  constructor(public activeRoute: ActivatedRoute, public productoService: ProductoService) {
+   
+    this.activeRoute.paramMap.subscribe(
+      param => {
+        let idParametro = param.get("idProducto") ?? "";
+        if (idParametro == "")
+          this.miProducto = new Producto();
+        else {
+          let productos: Array<Producto> = JSON.parse(localStorage.getItem("productos") ?? '[]');
+          this.miProducto = productos.filter(prod => prod._id.toString() == idParametro)[0]?? new Producto();
+        }
+      }
+    );
+  }
+
+  public log(argumento: any) 
+  {
+    console.info(argumento);
+  }
+
+  public guardar() {
+    let productos: Array<Producto> = this.productoService.Producto;
+    if(this.miProducto._id){
+      let anterior = productos.filter(prod => prod._id == this.miProducto._id)[0];
+      anterior._nombre = this.miProducto._nombre;
+      anterior._moneda = this.miProducto._moneda;
+      anterior._precio = this.miProducto._precio;
+      anterior._tipoProducto = this.miProducto._tipoProducto;
+    }else{
+      this.miProducto.id = productos.length + 1;
+      productos.push(this.miProducto);
+    }
+    this.productoService.saveProducto(productos);
+  }
+
+  public limpiar() {
+    this.miProducto = new Producto();
+  }
+}
